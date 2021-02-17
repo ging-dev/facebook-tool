f76e66a2577a080e43738b71facb9f590605f329
ging-dev <gingdz11012001@gmail.com>
2021-02-17 19:28:53 +0700

    Add scope

--- src/Commands/LoginCommand.php
+++ src/Commands/LoginCommand.php
@@ -19,6 +19,7 @@ class LoginCommand extends Command
     protected $queryData = [
         'client_id' => '124024574287414',
         'redirect_uri' => 'fbconnect://success',
+        'scope' => 'user_about_me,user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_activities,user_birthday,user_education_history,user_events,user_friends,user_games_activity,user_groups,user_hometown,user_interests,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationship_details,user_relationships,user_religion_politics,user_status,user_tagged_places,user_videos,user_website,user_work_history,email,manage_notifications,manage_pages,publish_actions,publish_pages,read_insights,read_page_mailboxes,read_stream,rsvp_event,read_mailbox',
         'response_type' => 'token',
     ];
 
